65248fa238be8c967ae2bd701ef204c61326421f
Harshith Jain <haajain2024@gmail.com>
2026-04-03 11:17:00 +0530

    Update branding assets, favicon setup, and navbar/footer logo imports

--- adhvagafrontend/src/Components/includes/Navbar.jsx
+++ adhvagafrontend/src/Components/includes/Navbar.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect, useRef } from "react";
 import { Link, NavLink, useLocation } from "react-router-dom";
 import { useSettings } from "../../context/SettingsContext";
-import logo from "../../../public/logo.png";
+import logo from "../../assets/logo.png";
 import StaggeredMenu from "./StaggeredMenu";
 import "./Navbar.css";
 
