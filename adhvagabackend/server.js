a7e5a229cedacb30845b3c8d8df4a1ae5e7622a4
Venugopal Reddy <venugopalreddy1895@gmail.com>
2026-07-24 09:24:34 +0530

    admin changes

--- adhvagabackend/server.js
+++ adhvagabackend/server.js
@@ -25,7 +25,8 @@ app.use(cors({
   methods: ["GET", "POST", "PUT", "DELETE"]
 }));
 
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 // ✅ Database connection with safety
 connectDB()
